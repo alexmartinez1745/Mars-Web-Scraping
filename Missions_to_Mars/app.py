9f632eb3a0b28b8f1ce9c8016266db80987bb2b3
Alex Martinez <alexmartinez1745@gmail.com>
2021-03-15 19:57:03 -0400

    updating flask app

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,10 +1,33 @@
 # Import libraries
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scrape_mars
 
 # Create Flask app
 app = Flask(__name__)
 
-# Create route
+# PyMongo for Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars")
+
+# Home route
+@app.route("/")
+def home():
+    mars_home = mongo.db.collection.find_one()
+    return render_template("index.html", mars = mars_home)
+
+# Route for scrape
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    mars_info = scrape_mars.scrape()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_info, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
 
 
 
